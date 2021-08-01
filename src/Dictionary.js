c769532812116d8672c395c719f92169e572af59
AleksRu21 <sasharudyak92@gmail.com>
2021-08-01 22:03:31 +0200

    Fixed link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,7 +13,9 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/sunset${keyword}`;
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
